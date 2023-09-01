aeb479d576176acad2b202205fe5e7b2f3304eac
MagnoVJ <magnovj@gmail.com>
2023-09-01 16:04:42 -0300

    01.09.2023-16.04

--- START.ps1
+++ START.ps1
@@ -1,3 +1,15 @@
 # Write this command: Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass to run ps1 scripts
+$binFolder = "bin"
+
+if (Test-Path $binFolder) {
+    Write-Host "PS bin folder already exists."
+}
+else {
+    New-Item -ItemType Directory -Path bin
+    Write-Host "PS bin folder created."
+}
+
+Write-Host "PS Compiling..."
+
 g++ src\main.cpp -obin\Main -std=c++17
 ./bin/Main
